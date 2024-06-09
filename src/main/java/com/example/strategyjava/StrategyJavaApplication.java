58ed49ca1508d959c55a2e39cce93b142e4e3265
Arthur Lopes <arthurlopr12@gmail.com>
2024-06-08 22:20:06 -0300

    feat: initial commit

---
+++ src/main/java/com/example/strategyjava/StrategyJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.strategyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/strategyjava/StrategyJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.strategyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
